b2185ffb657f6bb762b6119409d76dc2e88d4d9c
Pawan <pawan8090335@gmail.com>
2025-06-23 20:39:14 +0530

    push for sh command

--- vars/dotnetPublish.groovy
+++ vars/dotnetPublish.groovy
@@ -1,5 +1,5 @@
 def call(String project = 'YourProject.csproj', String output = 'publish') {
     stage('Publish') {
-        sh "dotnet publish ${project} --configuration Release --output ${output}"
+         sh "dotnet publish ${project} --configuration Release --output ${output}"
     }
 }
--- vars/dotnetTest.groovy
+++ vars/dotnetTest.groovy
@@ -1,5 +1,5 @@
 def call(String testProjectPath = '.\\tests') {
     stage('Test') {
-        sh "dotnet test ${testProjectPath} --configuration Release"
+         sh "dotnet test ${testProjectPath} --configuration Release"
     }
 }
